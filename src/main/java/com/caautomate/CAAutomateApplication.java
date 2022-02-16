2d3a76ea096f5911d66e1625faa9d5ad75016cd9
shahidlkshaikh <99790097+shahidlkshaikh@users.noreply.github.com>
2022-02-16 21:22:28 +0530

    Add files via upload

---
+++ src/main/java/com/caautomate/CAAutomateApplication.java
@@ -0,0 +1,13 @@
+package com.caautomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CAAutomateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CAAutomateApplication.class, args);
+	}
+
+}
